54f3ca605320db5b3bf2e8a0d6cbb90e17faf4e8
sriram-egen <sriram.pothuraju@egen.ai>
2024-08-22 09:44:02 +0000

    template syntax practice

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {ComponentBasicsComponent} from "./component-basics/component-basics.component"
 import { ProductComponent } from './product/product.component';
 import { ProductsComponent } from "./products/products.component";
+import { TemplateSyntaxComponent } from './template-syntax/template-syntax.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ComponentBasicsComponent, ProductComponent, ProductsComponent],
+  imports: [RouterOutlet, ComponentBasicsComponent, ProductComponent, ProductsComponent, TemplateSyntaxComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-basics/src/app/template-syntax/template-syntax.component.ts
+++ angular-basics/src/app/template-syntax/template-syntax.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { SizerComponent } from '../sizer/sizer.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-template-syntax',
   standalone: true,
-  imports: [],
+  imports: [SizerComponent, NgFor],
   templateUrl: './template-syntax.component.html',
   styleUrl: './template-syntax.component.css'
 })
 export class TemplateSyntaxComponent {
-
+  fontSizePx = 16;
+  phoneNumber ='';
+  callPhone(value:string){
+    this.phoneNumber = value 
+  }
+  heros=[
+    {
+      name: 'Sriram',
+      id: 1
+    },
+    {
+      name: 'Kumar',
+      id: 2
+    },
+    {
+      name: 'Pothuraju',
+      id: 3
+    },
+  ]
 }
